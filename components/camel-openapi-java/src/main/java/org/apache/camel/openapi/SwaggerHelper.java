34f86f306ac47e7f826046fe538f2323f04228ee
Freeman Fang <freeman.fang@gmail.com>
2019-11-25 13:01:44 -0500

    Open Api 3 support- add camel-openapi-java component-rename from swagger to openapi

---
+++ components/camel-openapi-java/src/main/java/org/apache/camel/openapi/SwaggerHelper.java
@@ -0,0 +1,117 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.openapi;
+
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.camel.util.FileUtil;
+
+import io.apicurio.datamodels.openapi.models.OasOperation;
+import io.apicurio.datamodels.openapi.models.OasPathItem;
+import io.apicurio.datamodels.openapi.v2.models.Oas20Document;
+import io.apicurio.datamodels.openapi.v2.models.Oas20SchemaDefinition;
+
+public final class SwaggerHelper {
+
+    private SwaggerHelper() {
+    }
+
+    public static String buildUrl(String path1, String path2) {
+        String s1 = FileUtil.stripTrailingSeparator(path1);
+        String s2 = FileUtil.stripLeadingSeparator(path2);
+        if (s1 != null && s2 != null) {
+            return s1 + "/" + s2;
+        } else if (path1 != null) {
+            return path1;
+        } else {
+            return path2;
+        }
+    }
+
+    /**
+     * Clears all the vendor extension on the swagger model. This may be needed as some API tooling does not support this.
+     */
+    public static void clearVendorExtensions(Oas20Document swagger) {
+        
+        if (swagger.getExtensions() != null) {
+            swagger.getExtensions().clear();
+        }
+
+        if (swagger.definitions.getDefinitions() != null) {
+            for (Oas20SchemaDefinition schemaDefinition : swagger.definitions.getDefinitions()) {
+                if (schemaDefinition.getExtensions() != null) {
+                    schemaDefinition.getExtensions().clear();
+                }
+            }
+        }
+        
+        if (swagger.paths != null) {
+            for (OasPathItem path : swagger.paths.getPathItems()) {
+                if (path.getExtensions() != null) {
+                    path.getExtensions().clear();
+                }
+                for (OasOperation op : getOperationMap(path).values()) {
+                    if (op.getExtensions() != null) {
+                        op.getExtensions().clear();
+                    }
+                }
+            }
+        }
+
+    }
+    
+    private static Map<HttpMethod, OasOperation> getOperationMap(OasPathItem path) {
+        Map<HttpMethod, OasOperation> result = new LinkedHashMap<HttpMethod, OasOperation>();
+
+        if (path.get != null) {
+            result.put(HttpMethod.GET, path.get);
+        }
+        if (path.put != null) {
+            result.put(HttpMethod.PUT, path.put);
+        }
+        if (path.post != null) {
+            result.put(HttpMethod.POST, path.post);
+        }
+        if (path.delete != null) {
+            result.put(HttpMethod.DELETE, path.delete);
+        }
+        if (path.patch != null) {
+            result.put(HttpMethod.PATCH, path.patch);
+        }
+        if (path.head != null) {
+            result.put(HttpMethod.HEAD, path.head);
+        }
+        if (path.options != null) {
+            result.put(HttpMethod.OPTIONS, path.options);
+        }
+
+        return result;
+    }
+    
+    enum HttpMethod {
+        POST,
+        GET,
+        PUT,
+        PATCH,
+        DELETE,
+        HEAD,
+        OPTIONS
+    }
+
+}
---
+++ components/camel-openapi-java/src/test/java/org/apache/camel/openapi/RestSwaggerReaderOverrideHostApiDocsTest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.openapi;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import org.apache.camel.impl.engine.DefaultClassResolver;
+import org.apache.camel.openapi.BeanConfig;
+import org.apache.camel.openapi.RestSwaggerReader;
+
+import org.junit.Test;
+
+import io.apicurio.datamodels.Library;
+import io.apicurio.datamodels.openapi.v2.models.Oas20Document;
+
+public class RestSwaggerReaderOverrideHostApiDocsTest extends RestSwaggerReaderApiDocsTest {
+
+    @Override
+    @Test
+    public void testReaderRead() throws Exception {
+        BeanConfig config = new BeanConfig();
+        config.setHost("localhost:8080");
+        config.setSchemes(new String[]{"http"});
+        config.setBasePath("/api");
+        config.setHost("http:mycoolserver:8888/myapi");
+        RestSwaggerReader reader = new RestSwaggerReader();
+
+        Oas20Document swagger = reader.read(context.getRestDefinitions(), null, config, context.getName(), new DefaultClassResolver());
+        assertNotNull(swagger);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        Object dump = Library.writeNode(swagger);
+        String json = mapper.writeValueAsString(dump);
+        System.out.println("the json is =====>" + json);
+
+        log.info(json);
+
+        assertTrue(json.contains("\"host\" : \"http:mycoolserver:8888/myapi\""));
+        assertTrue(json.contains("\"basePath\" : \"/api\""));
+
+        assertFalse(json.contains("\"/hello/bye\""));
+        assertFalse(json.contains("\"summary\" : \"To update the greeting message\""));
+        assertFalse(json.contains("\"/hello/bye/{name}\""));
+        assertTrue(json.contains("\"/hello/hi/{name}\""));
+
+        context.stop();
+    }
+
+}
