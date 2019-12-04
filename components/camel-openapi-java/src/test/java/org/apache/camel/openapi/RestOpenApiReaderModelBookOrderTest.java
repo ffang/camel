757e5fe85ebda884b390dd1ec591a86f03e936a1
Freeman Fang <freeman.fang@gmail.com>
2019-12-04 16:38:35 -0500

    Open Api 3 support- add camel-openapi-java component-add OpenApi V3 test for RestOpenApiReaderModelBookOrderTest

--- components/camel-openapi-java/src/test/java/org/apache/camel/openapi/RestOpenApiReaderModelBookOrderTest.java
+++ components/camel-openapi-java/src/test/java/org/apache/camel/openapi/RestOpenApiReaderModelBookOrderTest.java
@@ -89,5 +89,41 @@ public class RestOpenApiReaderModelBookOrderTest extends CamelTestSupport {
 
         context.stop();
     }
+    
+    @Test
+    public void testReaderReadV3() throws Exception {
+        BeanConfig config = new BeanConfig();
+        config.setHost("localhost:8080");
+        config.setSchemes(new String[] {"http"});
+        config.setBasePath("/api");
+        config.setTitle("Camel User store");
+        config.setLicense("Apache 2.0");
+        config.setLicenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html");
+        RestOpenApiReader reader = new RestOpenApiReader();
+
+        OasDocument openApi = reader.read(context.getRestDefinitions(), null, config, context.getName(), new DefaultClassResolver());
+        assertNotNull(openApi);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        Object dump = Library.writeNode(openApi);
+        String json = mapper.writeValueAsString(dump);
+        System.out.println("the json is =====>" + json);
+
+        log.info(json);
+
+        assertTrue(json.contains("\"url\" : \"http://localhost:8080/api\""));
+        assertTrue(json.contains("\"description\" : \"The order returned\""));
+        assertTrue(json.contains("\"BookOrder\""));
+        assertTrue(json.contains("\"LineItem\""));
+        assertTrue(json.contains("\"$ref\" : \"#/components/schemas/BookOrder"));
+        assertTrue(json.contains("\"$ref\" : \"#/components/schemas/LineItem"));
+        assertTrue(json.contains("\"x-className\""));
+        assertTrue(json.contains("\"format\" : \"org.apache.camel.openapi.BookOrder\""));
+        assertTrue(json.contains("\"format\" : \"org.apache.camel.openapi.LineItem\""));
+
+        context.stop();
+    }
 
 }
