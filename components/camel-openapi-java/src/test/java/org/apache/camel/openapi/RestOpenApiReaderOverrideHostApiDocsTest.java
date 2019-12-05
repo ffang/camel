97d0f19ced134c2e6d6190039fda463a36e9aff8
Freeman Fang <freeman.fang@gmail.com>
2019-12-05 11:12:13 -0500

    Open Api 3 support- add camel-openapi-java component-add OpenApi V3 test for RestOpenApiReaderOverrideHostApiDocsTest

--- components/camel-openapi-java/src/test/java/org/apache/camel/openapi/RestOpenApiReaderOverrideHostApiDocsTest.java
+++ components/camel-openapi-java/src/test/java/org/apache/camel/openapi/RestOpenApiReaderOverrideHostApiDocsTest.java
@@ -65,5 +65,37 @@ public class RestOpenApiReaderOverrideHostApiDocsTest extends RestOpenApiReaderA
 
         context.stop();
     }
+    
+    @Override
+    @Test
+    public void testReaderReadV3() throws Exception {
+        BeanConfig config = new BeanConfig();
+        config.setHost("localhost:8080");
+        config.setSchemes(new String[]{"http"});
+        config.setBasePath("/api");
+        config.setHost("http:mycoolserver:8888/myapi");
+        RestOpenApiReader reader = new RestOpenApiReader();
+
+        OasDocument openApi = reader.read(context.getRestDefinitions(), null, config, context.getName(), new DefaultClassResolver());
+        assertNotNull(openApi);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        Object dump = Library.writeNode(openApi);
+        String json = mapper.writeValueAsString(dump);
+        System.out.println("the json is =====>" + json);
+
+        log.info(json);
+
+        assertTrue(json.contains("\"url\" : \"http://http:mycoolserver:8888/myapi/api\""));
+        
+        assertFalse(json.contains("\"/hello/bye\""));
+        assertFalse(json.contains("\"summary\" : \"To update the greeting message\""));
+        assertFalse(json.contains("\"/hello/bye/{name}\""));
+        assertTrue(json.contains("\"/hello/hi/{name}\""));
+
+        context.stop();
+    }
 
 }
